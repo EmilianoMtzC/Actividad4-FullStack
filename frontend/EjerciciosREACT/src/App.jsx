d9fb6c04823c606ca8a0aeba23c0e62956dcdd92
EmilianoMtzC <jemtzc.2007@gmail.com>
2026-02-11 19:04:08 -0600

    Primeros ejercicios

---
+++ frontend/EjerciciosREACT/src/App.jsx
@@ -0,0 +1,13 @@
+import Saludo from './components/Saludo';
+
+function App() {
+    return (
+    <div>
+        <Saludo nombre="Juan"/>
+        <Saludo nombre="Pedro"/>
+        <Saludo nombre="Maria"/>
+    </div>
+    );
+}
+
+export default App;
